24b438071ef75775829253b826c7de7a09de947f
fabris29 <72633698+fabris29@users.noreply.github.com>
2020-10-20 12:50:21 -0300

    2019027490
    DentrE as soluções que adotei, destaco: Padrão de formatação, Organizei de melhor forma a classe mãe (Empregado) e as filhas Engenheiro/Vendedor. Dessa forma, economizei código a partir do uso de um construtor, um metodo get para retornar a saida que anteriormente estava na função main. Além disso, melhorei nomes de variaveis, usei limitadores de acessos e etc.

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -4,17 +4,16 @@
 class Engenheiro : public Empregado {
     public:
         Engenheiro( double salario , std::string nomeEmpregado , double horas , double projetos ){
-		    salarioHora = salario;
-		    nome = nomeEmpregado;
-		    horasTrabalhadas = horas;
-		    numProjetos = projetos;
+		        salarioHora = salario;
+		        nome = nomeEmpregado;
+		        horasTrabalhadas = horas;
+		        numProjetos = projetos;
 	      }
-        void get () override
-        {
-        std::cout << "Nome: " << nome << std::endl;
-        std::cout << "Salario Mes: "<< pagamentoMes() << std::endl;
-        std::cout << "Projetos: " << numProjetos << std::endl;
-        std::cout << std::endl;
+        void get () override{
+            std::cout << "Nome: " << nome << std::endl;
+            std::cout << "Salario Mes: "<< pagamentoMes() << std::endl;
+            std::cout << "Projetos: " << numProjetos << std::endl;
+            std::cout << std::endl;
         }
     private:
 	      int numProjetos;	
